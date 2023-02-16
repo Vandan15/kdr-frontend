ed39b79e30bf0448ed7683efdfcf3bf162b9a819
Vandan15 <vandankhamkar1@gmail.com>
2023-02-16 21:07:33 +0530

    add app script to store data in google sheet

--- components/home/CTA.js
+++ components/home/CTA.js
@@ -1,23 +1,52 @@
-// import axios from 'axios';
-import axios from 'axios';
+
+import React, { useState } from 'react'
 import Link from 'next/link';
-import React from 'react'
 import styles from './home.module.scss';
 
 
 export default function CTA() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+
   const handleSubmit = () => {
-    console.log('called')
-    axios.post('https://script.google.com/macros/s/AKfycbzOYP6AkETwRJAe2Xms0VhGtiREkqBd7DEN9IUS2QyvtjdreSO_bcxOJ_GZKe7HJ0gI/exec?action=addUser',{
-      name: 'test',
-      email: 'vandankhamkar2@gmail.com',
-      phone: '1234567890',
-      message: 'inquiry message',
-      formType: 'CTA'
-    }).then((res) => {
-      console.log("GSHEET RESPONSE", res.data)
-    }).catch(e => console.log(e))
-  }
+    setError('');
+    setSuccess('');
+    if (!name) {
+      setError('Invalid Name!');
+    } else if (!email) {
+      setError('Invalid Email!');
+    } else if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))) {
+      setError('Invalid Email!');
+    } else if (!phone) {
+      setError('Invalid Phone!');
+    } else if (phone.length < 10) {
+      setError('Invalid Phone!');
+    } else {
+      fetch("https://script.google.com/macros/s/AKfycbzOYP6AkETwRJAe2Xms0VhGtiREkqBd7DEN9IUS2QyvtjdreSO_bcxOJ_GZKe7HJ0gI/exec?action=addUser", {
+        method: "Post",
+        headers: { "Content-Type": "text/plain" },
+        body: JSON.stringify({
+          name: name,
+          email: email,
+          phone: phone,
+          message: 'Career Opportunities Inquiry',
+          formType: 'CTA'
+        })
+      }).then(
+        () => {
+          setSuccess("Response submitted!");
+          setName('');
+          setEmail('');
+          setPhone('');
+        }).catch((error) => {
+          console.error('Error:', error);
+        })
+    }
+  };
   return (
     <div className={styles.careerBg}>
       <div className='container'>
@@ -27,15 +56,17 @@ export default function CTA() {
         </div>
         <div className={styles.GridFrom}>
           <div className={styles.subGrid}>
-            <input placeholder='Enter Name' />
+            <input placeholder='Enter Name' value={name} onChange={e => setName(e.target.value)} />
           </div>
           <div className={styles.subGrid}>
-            <input placeholder='Enter Email' />
+            <input placeholder='Enter Email' value={email} onChange={e => setEmail(e.target.value)} />
           </div>
           <div className={styles.subGrid}>
-            <input placeholder='Enter Phone Number' />
+            <input placeholder='Enter Phone' value={phone} onChange={e => setPhone(e.target.value)} />
           </div>
         </div>
+        {error && <p className='text-danger text-center mt-3'>{error}</p>}
+        {success && <p className='text-success text-center mt-3'>{success}</p>}
         <div className={styles.buttonGrop} >
           <button onClick={() => handleSubmit()}>SUBMIT</button>
           <Link href='/career-opportunities'><button>VIEW OPPORTUNITIES</button></Link>
--- pages/contact-us.js
+++ pages/contact-us.js
@@ -1,12 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Footer from '../components/footer'
 import Header from '../components/header'
 import SEO from '../components/SEO'
 const img1 = '../assets/images/FirstBannerImag.png'
 export default function contact() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [message, setMessage] = useState('');
+
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
+
+    const handleSubmit = () => {
+        setError('');
+        setSuccess('');
+        if (!name) {
+            setError('Invalid Name!');
+        } else if (!email) {
+            setError('Invalid Email!');
+        } else if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))) {
+            setError('Invalid Email!');
+        } else if (!phone) {
+            setError('Invalid Phone!');
+        } else if (phone.length < 10) {
+            setError('Invalid Phone!');
+        } else if (!message) {
+            setError('Invalid Message!');
+        } else {
+            fetch("https://script.google.com/macros/s/AKfycbzOYP6AkETwRJAe2Xms0VhGtiREkqBd7DEN9IUS2QyvtjdreSO_bcxOJ_GZKe7HJ0gI/exec?action=addUser", {
+                method: "Post",
+                headers: { "Content-Type": "text/plain" },
+                body: JSON.stringify({
+                    name: name,
+                    email: email,
+                    phone: phone,
+                    message: message,
+                    formType: 'CONTACT-FORM'
+                })
+            }).then(
+                () => {
+                    setSuccess("Response submitted!");
+                    setName('');
+                    setEmail('');
+                    setPhone('');
+                    setMessage('');
+                }).catch((error) => {
+                    console.error('Error:', error);
+                })
+        }
+    };
     return (
         <>
-        <SEO title='Contact Us | KDR Groups'></SEO>
+            <SEO title='Contact Us | KDR Groups'></SEO>
             <Header />
             <div className='contact-us-wrapper '>
                 <div className='aboutsBanner'>
@@ -15,31 +61,29 @@ export default function contact() {
                 <div className='content-wrapper row container'>
                     <div className='col-12 col-md-6 d-md-flex justify-content-center order-2 order-md-1'>
                         <div className='form-wrapper'>
-                                <h3>Let's start conversation</h3>
-                                <p>Our responsible team will get back to you as soon as possible. Let us know if you have any query.</p>
-                                <div className='input-wrapper'>
-                                    <label>First Name</label>
-                                    <input type='text'></input>
-                                </div>
-                                <div className='input-wrapper'>
-                                    <label>Last Name</label>
-                                    <input type='text'></input>
-                                </div>
-                                <div className='input-wrapper'>
-                                    <label>Email</label>
-                                    <input type='text'></input>
-                                </div>
-                                <div className='input-wrapper'>
-                                    <label>Phone</label>
-                                    <input type='text'></input>
-                                </div>
-                                <div className='input-wrapper'>
-                                    <label>Message</label>
-                                    <textarea></textarea>
-                                </div>
-                                <div className='input-wrapper'>
-                                    <input type='submit' value='Send Message'></input>
-                                </div>
+                            <h3>Let's start conversation</h3>
+                            <p>Our responsible team will get back to you as soon as possible. Let us know if you have any query.</p>
+                            <div className='input-wrapper'>
+                                <label>Name</label>
+                                <input type='text' value={name} onChange={e => setName(e.target.value)}></input>
+                            </div>
+                            <div className='input-wrapper'>
+                                <label>Email</label>
+                                <input type='text' value={email} onChange={e => setEmail(e.target.value)}></input>
+                            </div>
+                            <div className='input-wrapper'>
+                                <label>Phone</label>
+                                <input type='text' value={phone} onChange={e => setPhone(e.target.value)}></input>
+                            </div>
+                            <div className='input-wrapper'>
+                                <label>Message</label>
+                                <textarea value={message} onChange={e => setMessage(e.target.value)}></textarea>
+                            </div>
+                            {error && <p className='text-danger text-center'>{error}</p>}
+                            {success && <p className='text-success text-center'>{success}</p>}
+                            <div className='input-wrapper'>
+                                <input type='button' value='Send Message' onClick={() => handleSubmit()}></input>
+                            </div>
                         </div>
                     </div>
                     <div className='col-12 col-md-6 d-flex align-items-center justify-content-center order-1 order-md-2 mb-5 mb-md-0'>
